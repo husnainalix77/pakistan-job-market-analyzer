a8d960a86640d4937c84f4be34c72e8c5a496bfc
Husnain Maroof <husnainalix77@gmail.com>
2026-07-17 16:18:49 +0500

    Phase 1: MySQL schema created - jobs and skills tables

---
+++ database/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE job_market_db;
+USE job_market_db;
+
+CREATE TABLE jobs(
+	job_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    city VARCHAR(100),
+    salary_min INT DEFAULT NULL,
+    salary_max INT DEFAULT NULL,
+    experience_years FLOAT DEFAULT NULL,
+    education VARCHAR(100),
+    category VARCHAR(100),
+    date_posted DATE,
+    date_scraped DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    
+);
+
+CREATE TABLE skills(
+	skill_id INT AUTO_INCREMENT PRIMARY KEY,
+    job_id INT NOT NULL,
+    skill_name VARCHAR(100) NOT NULL,
+    FOREIGN KEY (job_id) REFERENCES jobs(job_id) ON DELETE CASCADE
+)
+
